1c79dde874b7726042cd880c96c24de1618aa7f5
Gian Peña <gianpena2@gmail.com>
2026-09-06 14:06:28 -0400

    mitigate unhandled errors on nonexistent files

--- src/routes/[...path]/+page.server.ts
+++ src/routes/[...path]/+page.server.ts
@@ -1,9 +1,16 @@
 import { getFileTree } from '@/server';
 import type { PageServerLoad } from './$types';
 import { env } from '$env/dynamic/public';
+import { error } from '@sveltejs/kit';
+import { existsSync } from 'node:fs';
 
 export const load: PageServerLoad = async ({ params }) => {
 	const path = `${env.PUBLIC_HOME_PATH}/${params.path}`;
+
+	if (!existsSync(path)) {
+		error(404, 'Not found');
+	}
+
 	const files = await getFileTree(path);
 
 	return { files };
