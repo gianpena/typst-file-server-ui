f17e3b34de529e1a37213cf019d0e5e9ece8f4fb
Gian Peña <gianpena2@gmail.com>
2026-09-09 11:54:17 -0400

    don't load anything on prefetch

--- src/routes/[...path]/+page.svelte
+++ src/routes/[...path]/+page.svelte
@@ -2,18 +2,26 @@
 	import FileTree from '@/components/FileTree.svelte';
 	import FileViewer from '@/components/FileViewer.svelte';
 	import type { FileTreeNode } from '@/types';
+	import { onMount } from 'svelte';
 
 	const { data } = $props();
 
 	let fileCurrentlySelected: string = $state('');
+	let docUnavailable: boolean = $state(true);
 
+	onMount(() => {
+		const doc = document as Document & { prerendering?: boolean };
+		docUnavailable = doc.prerendering ?? false;
+	});
 </script>
 
-<div class="flex">
-	<FileTree fileTree={data.files as FileTreeNode[]} bind:fileCurrentlySelected />
-	{#if fileCurrentlySelected}
-		<FileViewer path={fileCurrentlySelected} />
-	{:else}
-		<p>No file selected</p>
-	{/if}
-</div>
+{#if !docUnavailable}
+	<div class="flex">
+		<FileTree fileTree={data.files as FileTreeNode[]} bind:fileCurrentlySelected />
+		{#if fileCurrentlySelected}
+			<FileViewer path={fileCurrentlySelected} />
+		{:else}
+			<p>No file selected</p>
+		{/if}
+	</div>
+{/if}
