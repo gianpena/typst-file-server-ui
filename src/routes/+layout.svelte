263955c9e7c4af41a604a13532a974dd29fb6d56
Gian Peña <gianpena2@gmail.com>
2026-09-14 13:28:14 -0400

    add password prompt at the layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,30 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 	
-	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
+	import { createQuery, QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
+	import { onMount } from 'svelte';
 	const queryClient = new QueryClient();
 
 	let { children } = $props();
+	let authenticated: boolean = $state(false);
+
+	onMount(() => {
+		const enteredPassword = window.prompt("Please enter the password to continue.");
+		const passwordMatchQuery = createQuery(() => ({
+			queryKey: ["password"],
+			queryFn: () => {
+				
+			}
+		}));
+	});
+
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-<QueryClientProvider client={queryClient}>
-	{@render children()}
-</QueryClientProvider>
+{#if authenticated}
+	<QueryClientProvider client={queryClient}>
+		{@render children()}
+	</QueryClientProvider>
+{:else}
+	<p>Forbidden</p>
+{/if}
