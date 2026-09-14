2508c9fac6332aa7dd860bd494368ba14880dd6d
Gian Peña <gianpena2@gmail.com>
2026-09-14 18:30:52 -0400

    revert layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,30 +2,13 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 	
-	import { createQuery, QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
-	import { onMount } from 'svelte';
+	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
 	const queryClient = new QueryClient();
 
 	let { children } = $props();
-	let authenticated: boolean = $state(false);
-
-	onMount(() => {
-		const enteredPassword = window.prompt("Please enter the password to continue.");
-		const passwordMatchQuery = createQuery(() => ({
-			queryKey: ["password"],
-			queryFn: () => {
-				
-			}
-		}));
-	});
-
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{#if authenticated}
-	<QueryClientProvider client={queryClient}>
-		{@render children()}
-	</QueryClientProvider>
-{:else}
-	<p>Forbidden</p>
-{/if}
+<QueryClientProvider client={queryClient}>
+	{@render children()}
+</QueryClientProvider>
