81a2ce795575afbaf2e89ae2c1540937003e7b91
Gian Peña <gianpena2@gmail.com>
2026-09-22 14:37:04 -0400

    move away from page prompt for auth
    still not secure yet but page prompts were very annoying

--- src/components/Auth.svelte
+++ src/components/Auth.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import { createQuery } from "@tanstack/svelte-query";
-	import { onMount } from "svelte";
 
 
     let { children } = $props();
     let password: string = $state("");
+    let form: HTMLFormElement | undefined = $state();
+
     const authQuery = createQuery(() => ({
         queryKey: ["passwordMatch", password],
         queryFn: async () => {
@@ -17,17 +18,40 @@
         }
     }));
 
-    onMount(() => {
-        password = window.prompt('Please enter the password.') ?? "";
+    $effect(() => {
+        if(!form) return;
+        form.onsubmit = (event) => {
+            event.preventDefault();
+            const formData = new FormData(form);
+            password = formData.get("pwd") as string;
+        };
     });
 
 </script>
 
-{#if authQuery.isLoading}
-    <p>Loading...</p>
-{:else if authQuery.isSuccess && authQuery.data}
-    {@render children()}
-{:else}
-    <p>Forbidden</p>
-{/if}
+{#snippet password_form(options: { error?: string, loading_message?: string } = {})}
+    {const {error, loading_message} = options;}
 
+    <form bind:this={form} class="flex flex-col gap-2 items-center justify-center w-screen h-screen pb-24">
+        <p class="text-gray-500">Please enter your password.</p>
+        <input value={password} name="pwd" class="p-2 rounded-sm border-solid border-2 border-gray-500 focus:outline-none" type="text">
+        {#if loading_message}
+            <p class="text-gray-500">{loading_message}</p>
+        {/if}
+        {#if error}
+            <p class="text-red-500">{error}</p>
+        {/if}
+    </form>
+{/snippet}
+
+{#if authQuery.isSuccess}
+    {#if authQuery.data}
+        {@render children()}
+    {:else}
+        {@render password_form({ error: password && "Incorrect password." })}
+    {/if}
+{:else if authQuery.isError}
+    {@render password_form({ error: "Something went wrong while authenticating you. Try again later." })}
+{:else if authQuery.isLoading}
+    {@render password_form({ loading_message: password && "Authenticating..." })}
+{/if}
